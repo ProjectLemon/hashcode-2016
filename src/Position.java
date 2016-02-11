1c6f9b21e49622656ba6b16ff5268e803bfffdd5
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2016-02-11 19:29:53 +0100

    Created and Implemented position

---
+++ src/Position.java
@@ -0,0 +1,41 @@
+/**
+ * Created by linuslagerhjelm on 16-02-11.
+ */
+public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Position position = (Position) o;
+
+        if (x != position.x) return false;
+        return y == position.y;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    public int getY() {
+
+        return y;
+    }
+
+    public int getX() {
+
+        return x;
+    }
+}
