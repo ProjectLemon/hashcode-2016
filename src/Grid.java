9cfc5a04d6af38bab3798c9f6cb6ce5c639f0a26
Fredrik Johansson <f95johansson@gmail.com>
2016-02-11 20:18:48 +0100

    Added data files

---
+++ src/Grid.java
@@ -0,0 +1,17 @@
+/**
+ * File: Grid.java
+ * Author: Fredrik Johansson
+ * Date: 2016-02-11
+ */
+
+public class Grid {
+
+    public Grid(int rows, int colums) {
+
+
+    }
+
+    public void addWarehouse(Warehouse warehouse) {
+
+    }
+}
